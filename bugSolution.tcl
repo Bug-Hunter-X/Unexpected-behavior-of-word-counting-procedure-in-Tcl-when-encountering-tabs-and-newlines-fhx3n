27bb595a4e99cdd8ed34e209cddb72ce32b83ce0
BugHunter-X <askzenai@gmail.com>
2025-01-24 20:10:07 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,17 @@
+proc count_words {text} {
+    set words 0
+    foreach word [split $text " "] {
+        if {[string length $word] > 0} {
+            incr words
+        }
+    }
+    return $words
+}
+
+puts [count_words "This is a test"] ; # Output: 4
+puts [count_words "This is a test with leading and trailing spaces"] ; # Output: 9
+puts [count_words ""] ; # Output: 0
+puts [count_words "    "] ; # Output: 0
+puts [count_words {This is a test with multiple \n newlines}] ; # Output: 9
+puts [count_words {This is a test with \t tabs}] ; # Output: 4
+puts [count_words {This is a test with multiple \t tabs\t}] ; # Output: 4
